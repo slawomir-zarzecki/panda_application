cd8c2095fb1e3728875fc9504fe14b00a82e42d4
Sławek <sasek.mail@gmail.com>
2021-03-25 18:01:18 +0000

    JenkinsFile

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -18,13 +18,13 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PandaApplicationSeleniumTest {
 
     private static WebDriver driver;
 
-    @LocalServerPort
-    private int port;
+  //  @LocalServerPort
+  //  private int port;
 
     @BeforeEach
     public void startup() throws InterruptedException, MalformedURLException {
@@ -43,7 +43,7 @@ public class PandaApplicationSeleniumTest {
       //  driver = new FirefoxDriver(options);
 
         // Pamiętaj, że aplikacja Spring musi działać!
-        driver.get(String.format("http://ubuntu-jenkins-slave:%s", port));
+        driver.get(String.format("http://192.168.44.44:%s", 8080));
 
         //Czekamy 2 sekundy
         Thread.sleep(2000);
